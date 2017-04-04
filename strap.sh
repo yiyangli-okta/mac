7175018c7d554f5cc4765e3d91e0823d6d1150c7
les-okta <les@okta.com>
2017-04-03 19:03:19 -0700

    Update strap.sh

--- strap.sh
+++ strap.sh
@@ -208,9 +208,9 @@ if command -v brew >/dev/null 2>&1; then
 else
   echo
   log "Installing Homebrew..."
-  HOMEBREW_PREFIX="/usr/local"
-  [ -d "$HOMEBREW_PREFIX" ] || sudo mkdir -p "$HOMEBREW_PREFIX"
-  sudo chown -R "$(logname):admin" "$HOMEBREW_PREFIX"
+  #HOMEBREW_PREFIX="/usr/local"
+  #[ -d "$HOMEBREW_PREFIX" ] || sudo mkdir -p "$HOMEBREW_PREFIX"
+  #sudo chown -R "$(logname):admin" "$HOMEBREW_PREFIX"
   yes '' | /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)";
 
   if [[ "$PATH" != *"/usr/local/bin"* ]]; then
