762b0d01e7e8172d2dfb0996eeb9425f5290984a
abdullahchougle-okta <34038751+abdullahchougle-okta@users.noreply.github.com>
2018-03-09 13:51:49 -0500

    Prefix sudo before com.apple.screensaver writes
    Writing to com.apple.screensaver requires sudo access in 10.13

--- strap.sh
+++ strap.sh
@@ -203,8 +203,8 @@ mkdir -p "$_STRAP_USER_DIR"
 logn "Checking security settings:"
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabled -bool false
 defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2JavaEnabledForLocalFiles -bool false
-defaults write com.apple.screensaver askForPassword -int 1
-defaults write com.apple.screensaver askForPasswordDelay -int 0
+sudo defaults write com.apple.screensaver askForPassword -int 1
+sudo defaults write com.apple.screensaver askForPasswordDelay -int 0
 sudo defaults write /Library/Preferences/com.apple.alf globalstate -int 1
 sudo launchctl load /System/Library/LaunchDaemons/com.apple.alf.agent.plist 2>/dev/null
 if [ -n "$STRAP_GIT_NAME" ] && [ -n "$STRAP_GIT_EMAIL" ]; then
